f8aae02a379bc613535ef7d32c07f1705aa54487
Aadesh1311 <100181537+Aadesh1311@users.noreply.github.com>
2022-05-23 17:08:50 +0530

    h
    h

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./addTask.module.css";
 
-const AddTask = () => {
+const AddTask = (props) => {
   // NOTE: do not delete `data-cy` key value pair
+  const[value,setValue]=useState("");
+  const handleChange = (e)=>{
+    setValue(e.target.value);
+  }
+  const addHandler=()=>{
+    props.onData(value)
+  }
   return (
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" type="text" />
-      <button data-cy="add-task-button"></button>
+      <div className="todoForm">
+      <input data-cy="add-task-input" type="text" onChange={handleChange}  />
+      <button data-cy="add-task-button" onClick={addHandler} className="btn">
+        +
+      </button>
+    </div>
     </div>
   );
 };
 
-export default AddTask;
+export default AddTask
+
+
+        
+      

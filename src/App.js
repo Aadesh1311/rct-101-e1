6de6e5627dd24c4e4687849d8779a1253a432744
Aadesh1311 <100181537+Aadesh1311@users.noreply.github.com>
2022-05-23 17:14:03 +0530

    h
    h

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import AddTask from "./components/AddTask/AddTask.jsx";
+import Tasks from "./components/Tasks/Tasks.jsx";
+import Data from "./data/tasks.json"
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
-  return <div>{/* Code Here */}</div>;
+
+  const[data,setData]=useState(Data);
+
+  const dataHandler=(title)=> {
+    var newdata={
+      "id":uuidv4(),
+      "text":title,
+      "done":true,
+      "count":1
+    };
+    setData([...data,newdata])
+  }
+  return <div>{/* Code Here */}
+  <AddTask onData={dataHandler}/>
+  {Data.map((task)=>
+      <Tasks key={task.id} task={task}/>
+  )}
+  </div>;
 }
 
 export default App;
+
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./tasks.module.css";
 
-const Tasks = () => {
+const Tasks = ({task}) => {
+  console.log(task.done)
+  const [isDone,setIsDone]=useState(task.done)
+  const[count,setCount]=useState(task.count)
   // NOTE: do not delete `data-cy` key value pair
   return (
-    <>
-      <ul data-cy="tasks" className={styles.tasks}>
+    
+
+    <div key={task.id}  className="tasks">
+      <ul data-cy="tasks" className={styles.tasks} key={task.id} >
+        <input type="checkbox" checked={isDone} onChange={(e)=>{setIsDone(!isDone)}}/>
+        
+        <div key={task.id} className={isDone ? styles.striked:""} >{task.text}</div>
+        
+        
+          <button onClick={()=>{setCount(count+1)}}>+</button>
+          <div>{count}</div>
+          <button onClick={()=>{setCount(count-1)}}>-</button>
+          
+          <button>delete</button>
+             
+        
         {/* Task List */}
       </ul>
       <div data-cy="tasks-empty" className={styles.empty}>
         {/* Show when No Tasks are present */}
       </div>
-    </>
+    </div>
   );
 };
 
